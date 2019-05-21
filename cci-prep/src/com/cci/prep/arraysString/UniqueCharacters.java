90352a172d1e0ee849c89fb9b877403d65ee08a2
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-21 08:50:07 +0530

    find unique characters from a string on different way

--- cci-prep/src/com/cci/prep/arraysString/UniqueCharacters.java
+++ cci-prep/src/com/cci/prep/arraysString/UniqueCharacters.java
@@ -10,10 +10,17 @@ public class UniqueCharacters {
 	
 	public static void main(String[] args) {
 		String str = "pranab";
-		System.out.println("Result : "+isUniqueChar(str));
+		System.out.println("Result : "+isUniqueChars(str));
+		System.out.println("Result : "+isUniqueChars1(str));
 	}
 
-	private static boolean isUniqueChar(String str) {
+	/**
+	 * @param str
+	 * @return true/false based on the input
+	 *  Create a HashSet of character type , Iterate through loop and add to the HashSet
+	 *  It will identify if there is already an existing character for the array in the set
+	 */
+	private static boolean isUniqueChars(String str) {
 		HashSet<Character> set = new HashSet<Character>();
 		boolean flag = true;
 		for(int i= 0;i<str.length();i++){
@@ -25,6 +32,25 @@ public class UniqueCharacters {
 		return flag;
 	}
 	
+	/**
+	 * @param str
+	 * @return true/false based on the input
+	 *  As we know we can't form a string of character more than 128 character alphabet.
+	 *  It's also okay to assume 256 characters in case of extended ASCII
+	 *  Create a boolean array of 128 character iterate the string through loop for each character
+	 *  if character is present return false else mark as true for the unique character in the array
+	 */
+	private static boolean isUniqueChars1(String str){
+		if(str.length() > 128)
+			return false;
+		boolean[] char_set = new boolean[128];
+		for(int i = 0; i<str.length();i++){
+			int val = str.charAt(i);
+			if(char_set[val])
+				return false;
+			char_set[val] = true;
+		}
+		return true;
+	}
 	
-
 }
