0f69f44155761d706245000b94df809c4a4c803b
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-06-12 13:37:17 +0530

    fetch employee list in main class

--- cci-prep/src/com/cic/prep/java8/LambdaComparator.java
+++ cci-prep/src/com/cic/prep/java8/LambdaComparator.java
@@ -3,10 +3,18 @@
  */
 package com.cic.prep.java8;
 
+import java.util.List;
+
+import com.cic.prep.pojo.Employee;
+
 /**
  * @author Pranab Kumar Sahoo
  *
  */
 public class LambdaComparator {
 
+	public static void main(String[] args) {
+		List<Employee> employeeList = Employee.getEmployees();
+		System.out.println(employeeList);
+	}
 }
