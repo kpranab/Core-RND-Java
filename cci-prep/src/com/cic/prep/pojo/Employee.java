faa83cf74fa26f7276332a140c25e319f8766a0a
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-06-12 13:34:13 +0530

    List of employee created with some predefined values

--- cci-prep/src/com/cic/prep/pojo/Employee.java
+++ cci-prep/src/com/cic/prep/pojo/Employee.java
@@ -3,6 +3,9 @@
  */
 package com.cic.prep.pojo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Pranab Kumar Sahoo
  *
@@ -62,5 +65,20 @@ public class Employee {
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", designation=" + designation + "]";
+	}
+
+	public static List<Employee> getEmployees() {
+
+		List<Employee> employeeList = new ArrayList<Employee>();
+
+		employeeList.add(new Employee(101, "Pranab", 70000, "Software Engineer"));
+		employeeList.add(new Employee(102, "Pramod", 40000, "DevOps Engineer"));
+		employeeList.add(new Employee(103, "Digbijoy", 50000, "Manager"));
+		employeeList.add(new Employee(104, "Mamuni", 2000, "Student"));
+		employeeList.add(new Employee(105, "Sony", 1000, "Heroin"));
+		employeeList.add(new Employee(106, "Chandan", 15000, "Farmer"));
+
+		return employeeList;
+
 	}
 }
