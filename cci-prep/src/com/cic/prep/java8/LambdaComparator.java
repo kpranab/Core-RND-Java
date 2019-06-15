ec654679e7c6e9ff7a9ff51c3a34c45b3ca7d7ea
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-06-15 10:49:38 +0530

    Sort employee by name before Java8 using comparator

--- cci-prep/src/com/cic/prep/java8/LambdaComparator.java
+++ cci-prep/src/com/cic/prep/java8/LambdaComparator.java
@@ -3,6 +3,8 @@
  */
 package com.cic.prep.java8;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.cic.prep.pojo.Employee;
@@ -15,6 +17,16 @@ public class LambdaComparator {
 
 	public static void main(String[] args) {
 		List<Employee> employeeList = Employee.getEmployees();
-		System.out.println(employeeList);
+		System.out.println("********************* Employee List Before sort *****************************\n");
+		employeeList.forEach(System.out::println);
+		System.out.println("\n______________________Sort by name before Java8_____________________________\n");
+		Collections.sort(employeeList,new Comparator<Employee>() {
+
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+		employeeList.forEach(emp -> System.out.println(emp));
 	}
 }
