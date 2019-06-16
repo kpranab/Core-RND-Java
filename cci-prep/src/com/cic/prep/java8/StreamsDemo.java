be22819d8289ab096738305ee0592c26bf8a97d3
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-06-16 14:12:46 +0530

    Java 8 stream filter usecase

--- cci-prep/src/com/cic/prep/java8/StreamsDemo.java
+++ cci-prep/src/com/cic/prep/java8/StreamsDemo.java
@@ -3,13 +3,23 @@
  */
 package com.cic.prep.java8;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.cic.prep.pojo.Employee;
+
 /**
  * @author Pranab Kumar Sahoo
  *
  */
 public class StreamsDemo {
+
 	public static void main(String[] args) {
-		
+
+		List<Employee> employeesList = Employee.getEmployeesList();
+		List<Employee> filteredList = employeesList.stream().filter(employee -> !"Pranab".equalsIgnoreCase(employee.getName())).collect(Collectors.toList());
+		System.out.println("**************************** Stream filter ******************************\n");
+		filteredList.forEach(System.out::println);
 	}
 
 }
