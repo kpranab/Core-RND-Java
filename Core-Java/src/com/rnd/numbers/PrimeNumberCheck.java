21e4c7dfea0b4d475dc0ee12f8579c02d360af40
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-01-06 15:34:22 +0530

    Display all prime number from a given range

--- Core-Java/src/com/rnd/numbers/PrimeNumberCheck.java
+++ Core-Java/src/com/rnd/numbers/PrimeNumberCheck.java
@@ -1,9 +1,20 @@
 package com.rnd.numbers;
 
+import java.util.Scanner;
+
 public class PrimeNumberCheck {
 
 	public static void main(String[] args) {
 		System.out.println("Is Prime number : "+PrimeNumberCheck.isPrime(10));
+
+		//Display prime numbers
+		//Scanner scanner = new Scanner(System.in);
+		int number = 50;//scanner.nextInt();
+		for(int i =2; i<number;i++) {
+			if(isPrime(i)) {
+				System.out.print(i+" ");
+			}
+		}
 	}
 	
 	public static boolean isPrime(int number) {
