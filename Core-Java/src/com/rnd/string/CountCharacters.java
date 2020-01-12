9ab5d4bd7f3be474a33f923e63bbe47051d31500
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-01-12 11:55:03 +0530

    String occcurance count using map

--- Core-Java/src/com/rnd/string/CountCharacters.java
+++ Core-Java/src/com/rnd/string/CountCharacters.java
@@ -1,5 +1,8 @@
 package com.rnd.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author pranab
  *
@@ -7,7 +10,8 @@ package com.rnd.string;
 public class CountCharacters {
 
 	public static void main(String[] args) {
-		CountCharacters.countChar("I am an Indian");
+		CountCharacters.countChar("I am an	Indian");
+		CountCharacters.countCharUsingMap("I am an	Indian");
 	}
 
 	private static void countChar(String str) {
@@ -30,6 +34,20 @@ public class CountCharacters {
 			str = str.replace("" + ch, "");
 
 		} while (chArr.length > 1);
+	}
+
+	private static void countCharUsingMap(String str) {
+		Map<Character,Integer> countMap = new HashMap<>();
+		for(int i=0; i<str.length();i++) {
+			char ch = str.charAt(i);
+			if(ch == ' ' || ch == '	')
+				continue;
+			if(countMap.containsKey(ch))
+				countMap.put(ch, countMap.get(ch)+1);
+			else
+				countMap.put(ch, 1);
+		}
+		countMap.forEach((k,v) -> System.out.println(k+ " -> "+v));
 	}
 
 }
