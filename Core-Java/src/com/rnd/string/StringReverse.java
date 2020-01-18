2ec7739c473ab7a54d01aa84fa4465ac0b38d060
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-01-18 23:48:38 +0530

    String reverse recursively

--- Core-Java/src/com/rnd/string/StringReverse.java
+++ Core-Java/src/com/rnd/string/StringReverse.java
@@ -8,6 +8,7 @@ public class StringReverse {
 
 	public static void main(String[] args) {
 		System.out.println(StringReverse.reverseString("Pranab"));
+		System.out.println(StringReverse.reverseStringRecursive("abcdef"));
 	}
 
 	private static String reverseString(String str) {
@@ -22,4 +23,13 @@ public class StringReverse {
 		return reverseString;
 	}
 
+	// Using recursion
+    private static String reverseStringRecursive(String str){
+        // validation & base case
+        if((str == null) || (str.length() <= 1)){
+            return str;
+        }
+        // recursive call
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
 }
