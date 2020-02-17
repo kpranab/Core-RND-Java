b00a547b45b9a73ebbc6d11b9a8d2ae77ae10183
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2020-02-17 22:50:39 +0530

    Binary search tree implementation

---
+++ Core-Java/src/com/rnd/algo/bst/BinaryTree.java
@@ -0,0 +1,22 @@
+package com.rnd.algo.bst;
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Node {
+
+	int value;
+	Node left;
+	Node right;
+
+	Node(int value) {
+		this.value = value;
+		left = null;
+		right = null;
+	}
+}
